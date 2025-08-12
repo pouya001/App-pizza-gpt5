d05b64b3180b37e3c84d2544eebb01698c10148c
pouya001 <129383490+pouya001@users.noreply.github.com>
2025-08-12 11:06:27 +0200

    Update next.config.js

--- pizzawoluwe-admin/next.config.js
+++ pizzawoluwe-admin/next.config.js
@@ -1,1 +1,4 @@
-const nextConfig = { experimental: { serverActions: true } }; module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+module.exports = nextConfig;
+
